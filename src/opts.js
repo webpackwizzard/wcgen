262841dac0c8254c16df0d87aced49ff828b91e8
saylestyler <saylestyler@gmail.com>
2019-05-18 12:18:05 -0400

    returns answers array

--- src/opts.js
+++ src/opts.js
@@ -1,109 +1,18 @@
 module.exports = options = [
   {
-    type: 'checkbox',
-    name: 'LESS',
+    type: 'list',
+    message: 'select CSS preprocessor',
+    name: 'cssPreprocessor',
     choices: [
-      "less-loader",
-      "less",
-      "style-loader",
-      "css-loader"
-    ],
-    generatedOut: `{
-                "test": /\.less$/,
-                "use": [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-          }`
+      "SASS", "LESS", "POSTCSS"
+    ]
   },
   {
-    type: 'checkbox',
-    name: 'Are you using CSS?',
+    type: 'list',
+    message: 'are you using a transpiler',
+    name: 'transpiler',
     choices: [
-      "style-loader",
-      "css-loader"
-    ],
-    generatedOut: ` {
-                "test": /\.css$/,
-                "use": [
-                    "style-loader",
-                    "css-loader"
-                ]
-          }`
-
-  },
-  {
-    type: 'checkbox',
-    name: 'Are you using SASS/SCSS?',
-    choices: [
-      "sass-loader",
-      "node-sass",
-      "style-loader",
-      "css-loader"
-    ],
-    generatedOutput: `{
-                "test": /\.scss$/,
-                "use": [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }`
-  },
-  {
-    type: 'checkbox',
-    name: 'Are you using POSTCSS?',
-    choices: [
-      "style-loader",
-      "css-loader",
-      "postcss-loader",
-      "precss",
-      "autoprefixer"
-    ],
-    generatedOutput: `
-      {
-          "test": /\.css$/,
-          "use": [
-              "style-loader",
-              "css-loader",
-              {
-                  "loader": "postcss-loader",
-                  "options": {
-                      "ident": "postcss",
-                      "plugins": (loader) => [
-                        require('postcss-import')(),
-                        require('postcss-cssnext')(),
-                        require('autoprefixer')(),
-                        require('cssnano')()
-                      ]
-                  }
-              }
-          ]
-      }`
-  },
-  {
-    name: 'ESLint'
-  },
-  {
-    name: 'JShint'
-  },
-  {
-    name: 'Babel - ES6+'
-  },
-  {
-    name: 'React JSX'
-  },
-  {
-    name: 'CoffeeScript'
-  },
-  {
-    name: 'TypeScript'
-  },
-  {
-    name: 'Polymer'
-  },
-  {
-    name: 'Vue'
+      "Babel", "TypeScript"
+    ]
   }
 ]
