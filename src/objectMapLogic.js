c01d7d737c37d5c2d4246afd1e85007c3930bc46
saylestyler <saylestyler@gmail.com>
2019-05-18 18:44:23 -0400

    generated webpack.config.js successfully, doubts concernting asynchronicity

--- src/objectMapLogic.js
+++ src/objectMapLogic.js
@@ -1,38 +1,47 @@
 const moduleConfig = require('./moduleLoadersConfig');
 const path = require('path');
-var fs = require('fs');
-
+const fs = require('fs');
 const util = require('util');
-// console.log(moduleConfig.LESS);
 
-const answers = ['CSS', 'LESS', 'React'];
-const sample = {
-  entry: './src/index.js',
-  output: {
-    path: "CODE:path.resolve(__dirname, 'dist')",
-    filename: 'bundle.js'
-  },
-  module: {}
-};
+const generateWebpackConfig = {}
 
-sample.module.rules = [];
+generateWebpackConfig.make = (answers) => {
+  console.log('moduleConfig', moduleConfig[answers[0]])
+  console.log('answers', answers)
 
-answers.forEach(element => {
-  sample.module.rules.push(moduleConfig[element]);
-});
+  const sample = {
+    entry: './src/index.js',
+    output: {
+      path: "CODE:path.resolve(__dirname, 'dist')",
+      filename: 'bundle.js'
+    },
+    module: {}
+  };
 
-fs.writeFile('testconfig.js', 'module.exports = ', err => {
-  if (err) throw err;
-});
-fs.appendFile(
-  'testconfig.js',
-  JSON.stringify(sample, null, 2)
-    .replace(/"CODE:/g, '')
-    .replace(/\)"/g, ')')
-    .replace(/\!"/g, '')
-    .replace(/"!/g, '')
-    .replace(/"\w+"(?=:)/g, val => val.replace(/"/g, '')),
-  err => {
+  sample.module.rules = [];
+
+  answers.forEach(element => {
+    sample.module.rules.push(moduleConfig[element]);
+  });
+
+  console.log('this is the sample', sample)
+
+  fs.writeFile('webpack.config.js', 'module.exports = ', err => {
     if (err) throw err;
-  }
-);
+  });
+
+  fs.appendFile(
+    'webpack.config.js',
+    JSON.stringify(sample, null, 2)
+      .replace(/"CODE:/g, '')
+      .replace(/\)"/g, ')')
+      .replace(/\!"/g, '')
+      .replace(/"!/g, '')
+      .replace(/"\w+"(?=:)/g, val => val.replace(/"/g, '')),
+    err => {
+      if (err) throw err;
+    }
+  );
+}
+
+module.exports = generateWebpackConfig
