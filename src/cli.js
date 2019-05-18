f1a46dcba06ca556872aa4ad0b56b2f02cb92785
Donald Blanc <dxb3662@rit.edu>
2019-05-17 22:09:06 -0400

    added ascii art

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
-
+const figlet = require('figlet');
+const chalk = require('chalk');
+const clear = require('clear');
 // business logic; called by binary in ../bin/webpack-wizard
 exports.init = () => {
-  console.log('test');
+  console.log(
+    chalk.yellow(
+      figlet.textSync('WebPackWizard', { horizontalLayout: 'full' })
+    )
+  );
+  console.log(
+    chalk.magenta(
+      'Pink Fairy Armadillos'
+    )
+  );
 }
