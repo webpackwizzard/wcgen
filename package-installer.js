fe03511a6497c904b9cc8b3592ab60e5f7e6d699
David Kim <davidkim024@gmail.com>
2019-05-20 15:44:47 -0400

    finished package-installer

--- package-installer.js
+++ package-installer.js
@@ -1,66 +1,81 @@
-const fs = require("fs");
-const path = require("path");
-const { spawn } = require("child_process")
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
 
 // const scaffold = require('./scaffold') // waiting for joel to push this
 
+const packageInstaller = {};
+
 const spawnNPMInit = () => {
-  const child = spawn("npm", ["init", "-y"])
-  process.stdin.pipe(child.stdin)
-  child.stdout.on("data", data => console.log(`child stdout:\n${data}`))
-}
+  const child = spawn('npm', ['init', '-y']);
+  process.stdin.pipe(child.stdin);
+  child.stdout.on('data', data => console.log(`child stdout:\n${data}`));
+};
 
 const spawnNPM = (pkg, isNew) => {
-    return spawn("npm", [isNew ? "install" : "update", pkg], {
-        stdio: "inherit"
-    });
-}
+  return spawn('npm', [isNew ? 'install' : 'update', pkg], {
+    stdio: 'inherit'
+  });
+};
 
 const spawnYarn = (pkg, isNew) => {
-  return spawn("yarn", [isNew ? "add" : "upgrade", pkg], {
-        stdio: "inherit"
-    });
-}
+  return spawn('yarn', [isNew ? 'add' : 'upgrade', pkg], {
+    stdio: 'inherit'
+  });
+};
 
 const SPAWN_FUNCTIONS = { npm: spawnNPM, yarn: spawnYarn };
 
 const getPackageManager = () => {
-    const hasLocalNPM = fs.existsSync(path.resolve(process.cwd(), "package-lock.json"));
-    const hasLocalYarn = fs.existsSync(path.resolve(process.cwd(), "yarn.lock"));
-    if (hasLocalNPM) {
-        return "npm";
-    }
-    else if (hasLocalYarn) {
-        return "yarn";
-    }
-    else if (spawn("yarn", [" --version"], { stdio: "ignore" }).error) {
-        return "npm";
-    }
-    else {
-        return "yarn";
-    }
-}
+  const hasLocalNPM = fs.existsSync(path.resolve(process.cwd(), 'package-lock.json'));
+  const hasLocalYarn = fs.existsSync(path.resolve(process.cwd(), 'yarn.lock'));
+  if (hasLocalNPM) {
+    return 'npm';
+  }
+  // else if (hasLocalYarn) {
+  //   return 'yarn';
+  // } else if (spawn('yarn', [' --version'], { stdio: 'ignore' }).error) {
+  //   return 'npm';
+  // } else {
+  //   return 'yarn';
+  // }
+};
 
 const getLocalPath = () => {
-    const manager = getPackageManager();
-    if (manager === "yarn") {
-        try {
-            let yarnDir = spawn("yarn", ["dir"]).stdout.toString().trim();
-            return path.join(yarnDir, "node_modules");
-        }
-        catch (e) { console.error(e) }
+  const manager = getPackageManager();
+  if (manager === 'yarn') {
+    try {
+      let yarnDir = spawn('yarn', ['dir'])
+        .stdout.toString()
+        .trim();
+      return path.join(yarnDir, 'node_modules');
+    } catch (e) {
+      console.error(e);
     }
-    return spawn("npm", ["root"])
-}
+  }
+  return spawn('npm', ['root']);
+};
 
-const spawnInstaller = (pkg) => {
-    const localPath = getLocalPath();
-    const pkgPath = path.resolve(localPath, pkg);
-    const packageManager = getPackageManager();
-    const isNew = !fs.existsSync(pkgPath);
+packageInstaller.spawnInstaller = pkg => {
+  // const localPath = getLocalPath();
 
-    return SPAWN_FUNCTIONS[packageManager](pkg, isNew);
-}
+  const pkgPath = path.resolve(`${process.cwd()}/node_modules`, pkg);
 
+  const packageManager = getPackageManager();
+  const isNew = !fs.existsSync(pkgPath);
+
+  console.log('Package to install: ', pkg);
+  // console.log('pkgPath: ', pkgPath);
+  // console.log('which package manager:', packageManager);
+  // console.log('Is is installed already: ', isNew);
+  // console.log(SPAWN_FUNCTIONS[packageManager]);
+  return SPAWN_FUNCTIONS[packageManager](pkg, isNew);
+};
+
+// packageInstaller.spawnInstaller('babel-loader');
+// console.log(spawnInstaller('babel-loader'));
+// ('babel-loader', true);
 // const pkgsToInstall = scaffold.getAnswers()
 // pkgsToInstall.forEach(p => spawnInstaller(p))
+
+module.exports = packageInstaller;
--- src/scaffold.js
+++ src/scaffold.js
@@ -1,16 +1,56 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
-const opts = require('./opts')
-const generateWebpackConfig = require('../src/objectMapLogic')
+const opts = require('./opts');
+const generateWebpackConfig = require('../src/objectMapLogic');
 
-const scaffold = {}
+// const moduleConfig = require('./moduleLoadersConfig');
+const packageInstaller = require('../package-installer');
+
+const scaffold = {};
+
+const mapModulesToLoaders = {
+  Babel: ['babel-loader', '@babel/preset-env', 'jshint-loader'],
+  React: ['babel-loader', '@babel/preset-env', '@babel/preset-react'],
+  CoffeeScript: ['coffee-loader'],
+  TypeScript: ['ts-loader'],
+  Vue: ['vue-loader'],
+  Polymer: ['babel-loader', 'polymer-webpack-loader'],
+  CSS: ['style-loader', 'css-loader'],
+  LESS: ['style-loader', 'css-loader', 'less-loader'],
+  SCSS: ['style-loader', 'css-loader', 'sass-loader'],
+  STYLUS: ['style-loader', 'css-loader', 'stylus-loader'],
+  ESLint: ['eslint-loader'],
+  JShint: ['jshint-loader'],
+  Image: ['file-loader'],
+  Fonts: ['file-loader'],
+  XML: ['xml-loader']
+};
 
 scaffold.beginPrompting = () => {
-  inquirer.prompt(opts)
-    .then(answers => {
-      generateWebpackConfig.make(Object.values(answers))
-    })
-}
+  inquirer.prompt(opts).then(answers => {
+    const answersArray = Object.values(answers);
+    generateWebpackConfig.make(answersArray);
 
-module.exports = scaffold
+    // npm package install
+
+    let packagesToInstall = ['webpack'];
+    answersArray.forEach(element => {
+      const packages = mapModulesToLoaders[element];
+      // console.log(packages);
+      packages.forEach(pkg => {
+        if (!packagesToInstall.includes(pkg)) {
+          if (pkg.includes('babel')) packagesToInstall.push('@babel/core');
+          packagesToInstall.push(pkg);
+        }
+      });
+    });
+    console.log('Installing... ', ...packagesToInstall);
+    packagesToInstall.forEach(pkg => {
+      packageInstaller.spawnInstaller(pkg);
+    });
+  });
+};
+
+// scaffold.beginPrompting();
+module.exports = scaffold;
