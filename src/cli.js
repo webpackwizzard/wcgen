cbf1c6f2fbace06738b794dadf864fc4dce29e84
Donald Blanc <dxb3662@rit.edu>
2019-05-21 08:48:05 -0400

    re-linting cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,12 @@ const chalk = require('chalk');
 const clear = require('clear');
 const scaffold = require('./scaffold');
 
-const cliScaffold = {}
+const cliScaffold = {};
 
 cliScaffold.init = () => {
-  console.log(chalk.yellow(figlet.textSync('WebPackWizard', { horizontalLayout: 'full',font: 'Ghost', })));
-  console.log("")
+  console.log(chalk.yellow(figlet.textSync('WebPackWizard', { horizontalLayout: 'full', font: 'Ghost' })));
   console.log(chalk.magenta('Pink Fairy Armadillos'));
-  scaffold.beginPrompting()
-}
+  scaffold.beginPrompting();
+};
 
-module.exports = cliScaffold
+module.exports = cliScaffold;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path'); 
+  module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          "file-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".jsx"
+    ]
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+}
