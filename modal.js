f770893b8b8a57fa43ce7a874233df26929640b1
Donald Blanc <dxb3662@rit.edu>
2019-05-21 10:04:42 -0400

    clearning

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById('myBtn');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
