d270c0438247486ce5446581a511b36425d47af5
Donald Blanc <dxb3662@rit.edu>
2019-05-21 09:22:59 -0400

    reLinted on scaffoled and refactored package loader section

--- src/scaffold.js
+++ src/scaffold.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
+const CLI = require('clui');
+const chalk = require('chalk');
 const opts = require('./opts');
 const generateWebpackConfig = require('../src/objectMapLogic');
-
-// const moduleConfig = require('./moduleLoadersConfig');
 const packageInstaller = require('../package-installer');
-const CLI = require('clui');
-const Spinner = CLI.Spinner;
-const Progress = CLI.Progress;
-const thisPercentBar = new Progress(20);
-const chalk = require('chalk');
+
+const thisPercentBar = new CLI.Progress(20);
 const scaffold = {};
 
 const mapModulesToLoaders = {
@@ -28,47 +25,42 @@ const mapModulesToLoaders = {
   JShint: ['jshint-loader'],
   Image: ['file-loader'],
   Fonts: ['file-loader'],
-  XML: ['xml-loader']
+  XML: ['xml-loader'],
 };
 
 scaffold.beginPrompting = () => {
-  inquirer.prompt(opts).then(answers => {
+  inquirer.prompt(opts).then((answers) => {
     const answersArray = Object.values(answers);
-    const status = new Spinner('Generating Webpack  Config please wait...');
+    const status = new CLI.Spinner('Generating Webpack  Config please wait...');
     status.start();
     generateWebpackConfig.make(answersArray);
 
     // npm package install
-    
+
     let percent = 0;
-    const intervalId = setInterval(function () {
+    const intervalId = setInterval(() => {
       percent += 0.01;
-      process.stdout.write(thisPercentBar.update(percent) + '\r');
+      process.stdout.write(`${thisPercentBar.update(percent)}  \r`);
       if (percent > 1.0) {
         clearInterval(intervalId);
         process.stdout.write('\n');
-        console.log(chalk.green("WebPack generated Successfully"));
+        console.log(chalk.green('WebPack generated Successfully'));
         status.stop();
         const packagesToInstall = ['webpack'];
-        answersArray.forEach(element => {
-          //console.log('elm',element)
-          if(mapModulesToLoaders[element] === undefined) return
-        const packages = mapModulesToLoaders[element];
-          // console.log(packages);
-          packages.forEach(pkg => {
+        answersArray.forEach(((element) => {
+          if (mapModulesToLoaders[element] === undefined) return;
+          const packages = mapModulesToLoaders[element];
+          packages.forEach(((pkg) => {
             if (!packagesToInstall.includes(pkg)) {
               if (pkg.includes('babel')) packagesToInstall.push('@babel/core');
               packagesToInstall.push(pkg);
             }
-          });
-        });
+          }));
+        }));
         console.log('Installing... ', ...packagesToInstall);
-        packagesToInstall.forEach(pkg => {
-          packageInstaller.spawnInstaller(pkg);
-        });
+        packagesToInstall.forEach(pkg => packageInstaller.spawnInstaller(pkg));
       }
     }, 60);
-
   });
 };
 
