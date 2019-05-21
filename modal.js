c79bf0466248a8c57fe72a428109613e99ea24cc
Donald Blanc <dxb3662@rit.edu>
2019-05-21 10:07:03 -0400

    clearning

--- modal.js
+++ modal.js
@@ -17,8 +17,8 @@ span.onclick = () => {
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
